ae7c8655dbc02e1a1dbd8f68006dc73caf64bf75
Dyusha12 <tamgdeya17@gmail.com>
2025-02-25 02:58:00 +0300

    Добавление информационных сообщений пользователю

--- FirstPythonApplication/FirstPythonApplication.py
+++ FirstPythonApplication/FirstPythonApplication.py
@@ -1,1 +1,8 @@
 print("Hello Python from Visual Studio!")
+s = "*" * 25
+print(s)
+print("Working on a new project")
+print(s)
+print("Initialization of the program...")
+print("The program is ready!")
+print(s)
